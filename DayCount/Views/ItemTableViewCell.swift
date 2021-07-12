0d2eec128ecd8bbeadc32183d15988ebc8011cb9
chagmn <sopg4@naver.com>
2021-07-12 17:33:48 +0900

    update ItemTableViewCell

--- DayCount/Views/ItemTableViewCell.swift
+++ DayCount/Views/ItemTableViewCell.swift
@@ -5,20 +5,22 @@
 //  Created by 창민 on 2021/05/21.
 //
 
+import RxCocoa
+import RxSwift
 import UIKit
 
-class ItemView: UIView {
+class ItemTableViewCell: UITableViewCell {
         
     var itemStackView: UIStackView = {
         let stackView = UIStackView()
         stackView.axis = .horizontal
         stackView.distribution = .fillProportionally
+        stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.backgroundColor = .systemGray6
         stackView.layer.borderColor = UIColor.systemGray.cgColor
         stackView.layer.borderWidth = 3.0
         stackView.layer.cornerRadius = 15
         stackView.clipsToBounds = true
-        stackView.translatesAutoresizingMaskIntoConstraints = false
         return stackView
     }()
     
@@ -55,61 +57,46 @@ class ItemView: UIView {
         return label
     }()
     
-    override init(frame: CGRect) {
-        super.init(frame: frame)
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
         self.translatesAutoresizingMaskIntoConstraints = false
+        
+        setupView()
+        setupLayout()
+    }
+    
+    func setupView(){
+        dateStackView.addArrangedSubview(date)
+        dateStackView.addArrangedSubview(ddaylabel)
         
         itemStackView.addArrangedSubview(title)
         itemStackView.addArrangedSubview(dateStackView)
         
-        dateStackView.addArrangedSubview(date)
-        dateStackView.addArrangedSubview(ddaylabel)
-        
-        addSubview(itemStackView)
-        
-        setLayout()
+        contentView.addSubview(itemStackView)
     }
     
-    // dday 계산 알고리즘
-    func calcDDay(date: String, isSwitchOn: Bool) -> Int{
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy년 MM월 dd일"
-        dateFormatter.locale = Locale(identifier: "ko_KR")
-        dateFormatter.timeZone = TimeZone(abbreviation: "UTC") as TimeZone?
-        let todayDate: Date = dateFormatter.date(from: dateFormatter.string(from: Date()))!
-        let textFieldDate: Date = dateFormatter.date(from: date)!
-        
-        var interval: TimeInterval
-        if isSwitchOn {
-            interval = todayDate.timeIntervalSince(textFieldDate)
-        } else {
-            interval = textFieldDate.timeIntervalSince(todayDate)
-        }
-        return Int(interval/86400)
-    }
-    
-    func setLayout(){
-        self.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width-20).isActive = true
-        self.heightAnchor.constraint(equalToConstant: UIScreen.main.bounds.height/10).isActive = true
-        
-        itemStackView.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
-        itemStackView.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
-        itemStackView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
-        itemStackView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
-        itemStackView.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+    func setupLayout(){
+        contentView.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width-20).isActive = true
+        contentView.heightAnchor.constraint(equalToConstant: UIScreen.main.bounds.height/10).isActive = true
+
+        itemStackView.leftAnchor.constraint(equalTo: contentView.leftAnchor).isActive = true
+        itemStackView.rightAnchor.constraint(equalTo: contentView.rightAnchor).isActive = true
+        itemStackView.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
+        itemStackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -5).isActive = true
         
         title.leftAnchor.constraint(equalTo: itemStackView.leftAnchor, constant: 10).isActive = true
+        title.rightAnchor.constraint(equalTo: dateStackView.leftAnchor, constant: 0).isActive = true
         
         dateStackView.topAnchor.constraint(equalTo: itemStackView.topAnchor).isActive = true
         dateStackView.bottomAnchor.constraint(equalTo: itemStackView.bottomAnchor).isActive = true
-        dateStackView.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -10).isActive = true
+        dateStackView.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: -10).isActive = true
         
-        date.topAnchor.constraint(equalTo: self.topAnchor, constant: 10).isActive = true
+        date.topAnchor.constraint(equalTo: itemStackView.topAnchor, constant: 10).isActive = true
         date.rightAnchor.constraint(equalTo: dateStackView.rightAnchor, constant: -10).isActive = true
         date.heightAnchor.constraint(equalToConstant: UIScreen.main.bounds.height/30).isActive = true
         
         ddaylabel.rightAnchor.constraint(equalTo: date.rightAnchor).isActive = true
-        ddaylabel.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -10).isActive = true
+        ddaylabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -10).isActive = true
     }
     
     required init?(coder: NSCoder) {
